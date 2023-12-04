57c64007d8aecb01af4bd4abeae1f864a7330e96
bskogman <bskogman1@outlook.com>
2023-12-04 16:29:10 -0600

    Day 3 pt2 done

--- Day03/Day03.py
+++ Day03/Day03.py
@@ -43,3 +43,45 @@ for s in symbols:
             #print(n[0])
 print("Solution for Part 1: "+str(part_sum))
 #fml I hate graphs
+
+#let's do pt 2 i guess
+cum_sum = 0
+for a in symbols:
+    cnt = 0
+    part_nums = [0,0]
+    if a[0] == '*':
+        x = a[1]
+        y = a[2]
+        for n in nums:
+            if x-1 == n[1] and n[2]-1 <= y <= n[3]+1:
+                if part_nums[0] != 0 and cnt == 1:
+                    part_nums[1] = int(n[0])
+                    cnt += 1
+                else:
+                    part_nums[0] = int(n[0])
+                    cnt += 1            
+            if x+1 == n[1] and n[2]-1 <= y <= n[3]+1:
+                if part_nums[0] != 0 and cnt == 1:
+                    part_nums[1] = int(n[0])
+                    cnt += 1
+                else:
+                    part_nums[0] = int(n[0])
+                    cnt += 1
+            if x == n[1] and y == n[3]+1:
+                if part_nums[0] != 0 and cnt == 1:
+                    part_nums[1] = int(n[0])
+                    cnt += 1
+                else:
+                    part_nums[0] = int(n[0])
+                    cnt += 1
+            if x == n[1] and y == n[2]-1:
+                if part_nums[0] != 0 and cnt == 1:
+                    part_nums[1] = int(n[0])
+                    cnt += 1
+                else:
+                    part_nums[0] = int(n[0])
+                    cnt += 1
+        if cnt == 2:
+            cum_sum += part_nums[0]*part_nums[1]
+        
+print("Solution for Part 2: "+str(cum_sum))            

e53dcce05112c08a85705d6222791c54e2ec1e84
bskogman <bskogman1@outlook.com>
2023-12-06 18:17:25 -0600

    Day 4 pt 2 done

--- Day04/Day04.py
+++ Day04/Day04.py
@@ -29,3 +29,26 @@ for s in scratchers:
     cum_sum += card_value
 
 print("Result for Part 1: "+str(cum_sum))
+
+num_cards = []
+for s in scratchers:
+    num_winners = 0
+    for n in s[2]:
+        try:
+            s[1].index(n)
+        except:
+            continue
+        else:
+            num_winners += 1
+    num_cards.append([int(s[0]),num_winners,1])
+
+for n in num_cards:
+    for i in range(n[2]):
+        for j in range(n[0],n[1]+n[0]):
+            num_cards[j][2] += 1
+
+tot_scratchers = 0
+for nc in num_cards:
+    tot_scratchers += nc[2]
+
+print("Result for Part 2: "+str(tot_scratchers))
